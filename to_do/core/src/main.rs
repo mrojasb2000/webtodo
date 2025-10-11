5a1f0b81cff94435fed415d945ed51994f8f2812
Mauricio Rojas <mrojasb2000@gmail.com>
2025-10-10 21:43:35 -0300

    chore: add clap dependencies

--- to_do/core/src/main.rs
+++ to_do/core/src/main.rs
@@ -1,6 +1,18 @@
-use std::env;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// first name of user
+    first_name: String,
+    /// last name of user
+    last_name: String,
+    /// age of the user
+    #[arg(short, long, default_value_t = 1)]
+    age: u8,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
     println!("{:?}", args);
 }
