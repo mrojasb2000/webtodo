0c56561cf0104d0fe0e22101353415da87243c25
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-10-13 16:03:32 -0300

    chore: created a pending and done struct

--- to_do/core/src/main.rs
+++ to_do/core/src/main.rs
@@ -2,25 +2,14 @@ mod enums;
 mod structs;
 mod api;
 
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// first name of user
-    #[arg(short, long)]
-    first_name: String,
-    /// last name of user
-    #[arg(short, long)]
-    last_name: String,
-    /// age of user
-    #[arg(short, long, default_value_t = 1)]
-    age: u8,
-}
+use structs::pending::Pending;
+use structs::done::Done;
 
 fn main() {
-   let args = Args::parse();
-   println!("{:?}", args.first_name);
-   println!("{:?}", args.last_name);
-   println!("{:?}", args.age);
+    let done = Done::new("shopping");
+    println!("{}", done.super_struct.title);
+    println!("{}", done.super_struct.status);
+    let pending = Pending::new("laundry");
+    println!("{}", pending.super_struct.title);
+    println!("{}", pending.super_struct.status);
 }
