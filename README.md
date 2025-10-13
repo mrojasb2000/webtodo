24e59937b38d5677ae18e6292f7d13d5960c132f
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-10-13 14:12:22 -0300

    chore: add nanoservices structs

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Running a command-line application
+```shell
+ cargo run -p core -- --help
+```
--- to_do/core/src/main.rs
+++ to_do/core/src/main.rs
@@ -1,6 +1,26 @@
-use std::env;
+mod enums;
+mod structs;
+mod api;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// first name of user
+    #[arg(short, long)]
+    first_name: String,
+    /// last name of user
+    #[arg(short, long)]
+    last_name: String,
+    /// age of user
+    #[arg(short, long, default_value_t = 1)]
+    age: u8,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+   let args = Args::parse();
+   println!("{:?}", args.first_name);
+   println!("{:?}", args.last_name);
+   println!("{:?}", args.age);
 }
