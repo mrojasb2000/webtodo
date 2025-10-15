9e60f962f8c91b5b4075c2a18d2d72b22c5afc5b
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-10-15 19:54:46 -0300

    chore: add argument parser and enums from string

--- to_do/core/src/enums.rs
+++ to_do/core/src/enums.rs
@@ -1,6 +1,8 @@
-
+use serde::{Serialize, Deserialize};
 use std::fmt;
 
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum TaskStatus {
     DONE,
     PENDING,
@@ -11,6 +13,16 @@ impl fmt::Display for TaskStatus {
         match &self {
             &Self::DONE => {write!(f, "DONE")},
             &Self::PENDING => {write!(f, "PENDING")},
+        }
+    }
+}
+
+impl TaskStatus {
+    pub fn from_string(status: &String) -> Result<TaskStatus, String> {
+        match status.to_uppercase().as_str() {
+            "DONE" => Ok(TaskStatus::DONE),
+            "PENDING" => Ok(TaskStatus::PENDING),
+            _ => Err(format!("Invalid status: {}", status))
         }
     }
 }
--- to_do/core/src/main.rs
+++ to_do/core/src/main.rs
@@ -2,8 +2,22 @@ mod enums;
 mod structs;
 mod api;
 use api::basic_actions::creates::create;
+use crate::enums::TaskStatus;
+use clap::Parser;
 
-fn main() {
-   let to_do_item = create("washing", enums::TaskStatus::PENDING);
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    title: String,
+    #[arg(short, long)]
+    status: String,
+}
+
+fn main() -> Result<(), String> {
+    let args = Args::parse();
+    let status_enum = TaskStatus::from_string(&args.status)?;
+    let to_do_item = create(&args.title, status_enum)?;
     println!("{}", to_do_item);
+    Ok(())
 }
