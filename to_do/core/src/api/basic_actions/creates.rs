60555d8d56ee55fc30a027fdce91fb235d7e920a
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-10-15 19:52:53 -0300

    chore: create function in api

--- to_do/core/src/api/basic_actions/creates.rs
+++ to_do/core/src/api/basic_actions/creates.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use dal::json_file::save_one;
 
 use crate::structs::{
     done::Done,
@@ -21,9 +22,14 @@ impl fmt::Display for ItemTypes {
     }
 }
 
-pub fn create(title: &str, status: TaskStatus) -> ItemTypes {
+pub fn create(title: &str, status: TaskStatus) -> Result<ItemTypes, String> {
+    let _ = save_one(&title.to_string(), &status)?;
     match status {
-        TaskStatus::PENDING => ItemTypes::Pending(Pending::new(title)),
-        TaskStatus::DONE => ItemTypes::Done(Done::new(title)),
+        TaskStatus::PENDING => {
+            Ok(ItemTypes::Pending(Pending::new(title)))
+        },
+        TaskStatus::DONE => {
+            Ok(ItemTypes::Done(Done::new(title)))
+        },
     }
 }
