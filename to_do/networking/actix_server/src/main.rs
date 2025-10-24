a2b3da13cb98fc8f47e337f0f38428184ee15786
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-10-24 13:28:45 -0300

    chore: add actix web server

---
+++ to_do/networking/actix_server/src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, App, HttpServer, Responder, HttpResponse, HttpRequest};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    HttpResponse::Ok().body(format!("Hello, {}!", name))
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
+    })
+    .workers(4)
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
