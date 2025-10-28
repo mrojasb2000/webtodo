dbe9115675dbb2ac38b4cf608862136193563fde
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-10-28 13:54:42 -0300

    chore: add get by name endpoint

--- nanoservices/to_do/core/src/api/basic_actions/get.rs
+++ nanoservices/to_do/core/src/api/basic_actions/get.rs
@@ -1,7 +1,16 @@
 use dal::json_file::get_all as get_all_handle;
 use crate::structs::{ToDoItem, AllToDoItems};
-use glue::errors::NanoServiceError;
+use glue::errors::{NanoServiceError, NanoServiceErrorStatus};
 
 pub async fn get_all() -> Result<AllToDoItems, NanoServiceError> {
     Ok(AllToDoItems::from_hashmap(get_all_handle::<ToDoItem>()?))
+}
+
+pub async fn get_by_name(name: &str) -> Result<ToDoItem, NanoServiceError> {
+    Ok(get_all_handle::<ToDoItem>()?
+       .remove(name) // Removes a key from the map, returning the value at the key if the key was previously in the map.
+       .ok_or(NanoServiceError::new(
+        format!("Item with name {} not found", name),
+        NanoServiceErrorStatus::NotFound,
+    ))?)
 }
